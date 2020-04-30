65cdc325a08c4c0885200d6209a1e1fb4fff929c
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-30 16:52:12 -0400

    Share data among multiple subscribers with Subjects.

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-import { of } from 'rxjs';
+import { Subject, interval } from 'rxjs';
+import { tap, take } from 'rxjs/operators';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
+const observer = {
+  next: (val) => console.log('next', val),
+  error: (err) => console.log('error', err),
+  complete: () => console.log('complete'),
+};
+
+const subject$ = new Subject();
+
+const subscription = subject$.subscribe(observer);
+
+// subject$.next('Hello');
+
+const subscription2 = subject$.subscribe(observer);
+
+// subject$.next('World');
+
+const interval$ = interval(2000).pipe(
+  tap((value) => console.log('new interval', value))
+);
+
+interval$.pipe(take(5)).subscribe(observer);
+interval$.pipe(take(5)).subscribe(observer);
+
+/**
+ * it logs
+ * new interval 0
+ * next 0
+ * new interval 0
+ * next 0
+ *
+ * new interval 1
+ * next 1
+ * new interval 1
+ * next 1
  */
-of('Hello', 'RxJS').subscribe(console.log);
+
+interval$.pipe(take(5)).subscribe(subject$);
+
+/**
+ * it logs:
+ * new interval 0
+ * next 0
+ * next 0
+ *
+ * new interval 1
+ * next 1
+ * next 1
+ */
