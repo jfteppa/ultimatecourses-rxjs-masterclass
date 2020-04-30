73c4712ad183de440faa5538accc317a98a9cd0d
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-30 17:46:51 -0400

    Lab: Manage application loading state with Subjects.

--- index.js
+++ index.js
@@ -1,63 +1,38 @@
-import { Subject, interval } from 'rxjs';
-import { tap, take } from 'rxjs/operators';
+// import { Subject } from 'rxjs';
+import { loadingService } from './loadingService';
 
-const observer = {
-  next: (val) => console.log('next', val),
-  error: (err) => console.log('error', err),
-  complete: () => console.log('complete'),
-};
+const loadingOverlay = document.getElementById('loading-overlay');
 
-const subject$ = new Subject();
+// loadingOverlay.classList.add('open');
 
-const subscription = subject$.subscribe(observer);
+/*
+const loading$ = new Subject();
 
-// subject$.next('Hello');
+loading$.subscribe((isLoading) => {
+  if (isLoading) {
+    loadingOverlay.classList.add('open');
+  } else {
+    loadingOverlay.classList.remove('open');
+  }
+});
 
-const subscription2 = subject$.subscribe(observer);
-
-// subject$.next('World');
-
-const interval$ = interval(2000).pipe(
-  tap((value) => console.log('new interval', value))
-);
-
-const subscibe1 = interval$.pipe(take(5)).subscribe(observer);
-const subscibe2 = interval$.pipe(take(5)).subscribe(observer);
-
-subscibe1.add(subscibe2);
+loading$.next(true);
 
 setTimeout(() => {
-  console.log(`unsubscribing all observer's subscriptions`);
-  subscibe1.unsubscribe();
-  // subscibe2.unsubscribe();
-}, 5000);
-/**
- * it logs
- * new interval 0
- * next 0
- * new interval 0
- * next 0
- *
- * new interval 1
- * next 1
- * new interval 1
- * next 1
- */
+  loading$.next(false);
+}, 3000);
+*/
 
-const subscibe3 = interval$.pipe(take(5)).subscribe(subject$);
+loadingService.loadingStatus.subscribe((isLoading) => {
+  if (isLoading) {
+    loadingOverlay.classList.add('open');
+  } else {
+    loadingOverlay.classList.remove('open');
+  }
+});
+
+loadingService.showLoading();
 
 setTimeout(() => {
-  console.log(`unsubscribing all subject's subscriptions`);
-  subscibe3.unsubscribe();
-}, 5000);
-
-/**
- * it logs:
- * new interval 0
- * next 0
- * next 0
- *
- * new interval 1
- * next 1
- * next 1
- */
+  loadingService.hideLoading();
+}, 3000);
