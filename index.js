54b14112592a856c2da9f42c672b7e9d60a0bb29
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-30 21:16:22 -0400

    Replay history to new subscribers with ReplaySubjects.

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-import { ObservableStore } from './store';
+import { ReplaySubject } from 'rxjs';
 
-const strore = new ObservableStore({
-  user: 'teppa',
-  isAuthenticated: false,
-});
+const observer = {
+  next: (val) => console.log('next', val),
+  error: (err) => console.log('error', err),
+  complete: () => console.log('complete'),
+};
 
-strore.stateChanges().subscribe((state) => {
-  console.log('state changed', state);
-});
+/**
+ * ReplaySubject does not have an inital value
+ */
 
-strore.stateKeyChanges('user').subscribe((state) => {
-  console.log('state Key user changed', state);
-});
+/*
+ * ReplaySubject's accept an optional argument, the number
+ * of previously emitted values you wish to 'replay'
+ * on subscription. These values will be emitted in sequence
+ * beginning with the most recent, to any late subscribers.
+ *
+ * By default, if no arguments are supplied all values are replayed.
+ */
+const subject = new ReplaySubject();
 
-setTimeout(() => {
-  strore.updateState({
-    user: 'joe',
-  });
-}, 2000);
+subject.next('Hello');
+subject.next('World');
+subject.next('Goodbay');
 
-setTimeout(() => {
-  strore.updateState({
-    isAuthenticated: true,
-  });
-}, 4000);
+subject.subscribe(observer); // Hello, World, Goodbay
+
+const subject2 = new ReplaySubject(3);
+
+subject2.next('1');
+subject2.next('2');
+subject2.next('3');
+subject2.next('4');
+subject2.next('5');
+
+subject2.subscribe(observer); // 3, 4, 5, 6, 7
+
+subject2.next('6');
+subject2.next('7');
