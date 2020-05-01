e20a4b820c0a6a568b392f4fa4f1b573f71519b4
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-30 22:16:15 -0400

    Deliver the last value on complete with AsyncSubjects.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import { fromEvent, interval } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
-import { shareReplay, mergeMapTo, share, take, tap } from 'rxjs/operators';
+import { AsyncSubject } from 'rxjs';
 
 const observer = {
   next: (val) => console.log('next', val),
@@ -8,58 +6,27 @@ const observer = {
   complete: () => console.log('complete'),
 };
 
-const ajax$ = ajax('https://api.github.com/users/octocat');
-
-const click$ = fromEvent(document, 'click');
+/*
+ * AsyncSubject's only emit the final value on completion.
+ */
+const subject = new AsyncSubject();
 
 /*
- * shareReplay turns a unicast observable into multicasted
- * observable, utilizing a ReplaySubject behind the scenes.
- *
- * In this example, we are mapping any clicks to an ajax
- * request, sharing the response.
+ * For instance, let's create a few subscribers here...
  */
-
-const sharedClick$ = click$.pipe(
-  mergeMapTo(interval(1000).pipe(take(5))),
-  // mergeMapTo(ajax$),
-  /*
-   * By default shareReplay shares all old values, like
-   * a standard ReplaySubject. In this case, we only want
-   * to share the most recent response.
-   */
-
-  /**
-   * this makes new subscriber not to trigger the ajax
-   * more than 1 time, comment out to see the network after 1 click
-   */
-  // shareReplay()
-
-  // only the n last values
-  // shareReplay(2)
-
-  /**
-   * the 2nd argument sets how long are all values available
-   */
-  shareReplay(2, 2000)
-);
-
-sharedClick$.subscribe(observer);
-
-// it does not trigger the ajax after adding shareReplay()
-sharedClick$.subscribe(observer);
+const subscription = subject.subscribe(observer);
+const secondSubscription = subject.subscribe(observer);
 
 /*
- * Late subscribers will be replayed old value(s).
+ * next 4 values to AsyncSubject, nothing is emitted to observers.
  */
-setTimeout(() => {
-  console.log('subscribing');
+subject.next('Hello');
+subject.next('World');
+subject.next('Goodbye!');
+subject.next('World!');
 
-  /**
-   * it does not trigger the ajax request
-   *
-   * but if we click more than 1 time
-   * it will get all the data
-   */
-  sharedClick$.subscribe(observer);
-}, 5000);
+/*
+ * Once the subject completes, the last value, in this case
+ * 'World!' is emitted.
+ */
+subject.complete();
