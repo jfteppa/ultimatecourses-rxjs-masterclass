daaf5fd4f0b121866280c3adeb8e426d02a0c531
Juan Felipe Teppa <jteppa@rootstack.com>
2020-05-04 13:44:21 -0400

    Schedule tasks before browser repaint with the Animation Frame Scheduler.

--- index.js
+++ index.js
@@ -1,97 +1,37 @@
-import { asyncScheduler, asapScheduler, of, range } from 'rxjs';
-import { observeOn, subscribeOn, tap } from 'rxjs/operators';
+import { animationFrameScheduler, interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
-const observer = {
-  next: (val) => console.log('next', val),
-  error: (err) => console.log('error', err),
-  complete: () => console.log('complete'),
-};
-
+const ball = document.getElementById('ball');
 /*
- * The asapScheduler executes tasks asynchronously but
- * 'as quickly as possible', similar to microtasks.
- * For instance, even though our task scheduled with
- * the asapScheduler appears after the asyncScheduler
- * task, it will be executed before, but not before the
- * synchronous console.log. This is the same behavior
- * you would see with Promise.resolve or queueMicrotask.
+ * The animationFrameScheduler let's you schedule
+ * tasks before browser repaint
+ * to help create smooth animations
  */
 
-asyncScheduler.schedule(() => console.log('asyncScheduler'));
+// animationFrameScheduler.schedule(function(position){
+//   ball.style.transform = `translate3d(0, ${position}px, 0`;
+//   if(position <= 300) {
 
-setTimeout(() => {
-  console.log('setTimeout');
-}, 0);
+//     /**
+//      * because we are using a function keyword and not a arrow function
+//      * the context of the this keyword will be the currently executing action itselt
+//      */
+//     this.schedule(position + 1)
+//   }
+// },
+// /**
+//  * 0 for the delay, if we pass any other value this will simple default
+//  * to the asyncScheduler
+//  */
+// 0,
+// /**
+//  * for the state property we are passing 0 as this is the starting position
+//  * for the element on the page
+//  */
+// 0);
 
-asyncScheduler.schedule(() => console.log('asyncScheduler 2'));
-
-setTimeout(() => {
-  console.log('setTimeout2');
-}, 0);
-
-/**
- * async will log in order but after the synchronous.
- */
-
-Promise.resolve('from Promise').then(console.log);
-
-queueMicrotask(() => console.log('from queueMicrotask'));
-
-asapScheduler.schedule(() => console.log('from asapScheduler'));
-
-/**
- * asyncScheduler, queueMicrotask and Promise they log in order,
- * if we change the order it will print them in the new order and
- * they will print after the synchronous but before the asyncScheduler
- */
-
-console.log('synchronous');
-
-/*
- * Like other schedulers it can be provided as an
- * argument to most static operators, or by using the observeOn
- * or subscribeOn operators.
- */
-// as argument to static operator
-
-range(1, 5).subscribe(console.log); // synchronous
-
-range(1, 5, asapScheduler).subscribe(console.log); // asapScheduler
-
-// of(1,2,3, asapScheduler).subscribe(observer);
-
-// using observeOn
-// of(1,2,3).pipe(
-//   // logging values before scheduler
-//   tap(observer),
-//   observeOn(asapScheduler)
-// ).subscribe(observer);
-
-// using subscribeOn
-// of(1,2,3).pipe(
-//   tap(observer),
-//   subscribeOn(asapScheduler)
-// ).subscribe(observer);
-
-/**
- * for long running operations it will block the ui until they complete
- * for this example we can see the browser pauses and then it
- * finally renders with the final value, this is because the browser is locked
- * waiting for the Microtask to finish before updating
- */
-
-const counter = document.getElementById('counter');
-const counter2 = document.getElementById('counter2');
-
-range(1, 100000, asapScheduler).subscribe((val) => {
-  counter.innerHTML = val;
-});
-
-// if we remove the asapScheduler we will have the same result
-
-// if we switch to the asyncScheduler
-// we will see the update of the numbers 1by 1
-
-range(1, 1000, asyncScheduler).subscribe((val) => {
-  counter2.innerHTML = val;
-});
+interval(0, animationFrameScheduler)
+  .pipe(takeWhile((val) => val <= 300))
+  .subscribe((val) => {
+    ball.style.transform = `translate3d(0, ${val}px, 0`;
+  });
