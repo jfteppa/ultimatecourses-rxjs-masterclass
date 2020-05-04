b6b42a5106ee5453b0d73154464c0d0704d754cb
Juan Felipe Teppa <jteppa@rootstack.com>
2020-05-04 13:31:28 -0400

    Defer task execution with the Asap Scheduler.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { asyncScheduler, of, fromEvent } from 'rxjs';
+import { asyncScheduler, asapScheduler, of, range } from 'rxjs';
 import { observeOn, subscribeOn, tap } from 'rxjs/operators';
 
 const observer = {
@@ -8,94 +8,90 @@ const observer = {
 };
 
 /*
- * The asyncScheduler lets you schedule tasks asynchronously,
- * similar to a setTimeout. All schedulers have a signature
- * of work, delay, state, but providing a delay for any other
- * scheduler will simply default it to the asyncScheduler
- * behind the scenes.
- *
- * The schedule call returns a subscription, so if you need
- * to cancel work before it is performed you can simply
- * unsubscribe, similar to observables.
+ * The asapScheduler executes tasks asynchronously but
+ * 'as quickly as possible', similar to microtasks.
+ * For instance, even though our task scheduled with
+ * the asapScheduler appears after the asyncScheduler
+ * task, it will be executed before, but not before the
+ * synchronous console.log. This is the same behavior
+ * you would see with Promise.resolve or queueMicrotask.
  */
 
-// work, delay?, state?
-// asyncScheduler.schedule(() => console.log('Hellow World'));
-// asyncScheduler.schedule(() => console.log('Hellow World 2s'), 2000);
-/* asyncScheduler.schedule(
-  // work
-  console.log,
-  // delay
-  3000,
-  // state
-  'Hello world 3s'
-); */
+asyncScheduler.schedule(() => console.log('asyncScheduler'));
 
 setTimeout(() => {
   console.log('setTimeout');
 }, 0);
 
-console.log('sync');
+asyncScheduler.schedule(() => console.log('asyncScheduler 2'));
 
-const sub = asyncScheduler.schedule(console.log, 1000, 'Hello world 1s');
+setTimeout(() => {
+  console.log('setTimeout2');
+}, 0);
 
-// it does log the HW 3s because we unsubscribe before the 1s delay
-sub.unsubscribe();
+/**
+ * async will log in order but after the synchronous.
+ */
+
+Promise.resolve('from Promise').then(console.log);
+
+queueMicrotask(() => console.log('from queueMicrotask'));
+
+asapScheduler.schedule(() => console.log('from asapScheduler'));
+
+/**
+ * asyncScheduler, queueMicrotask and Promise they log in order,
+ * if we change the order it will print them in the new order and
+ * they will print after the synchronous but before the asyncScheduler
+ */
+
+console.log('synchronous');
 
 /*
- * Most static creation operators accept a scheduler as
- * the last argument. For instance, if we want to emit
- * values from of asynchronously, we can supply the
- * asyncScheduler as the last argument.
- *
- * Output: 1,2,3,4,5,6,7,8,9
+ * Like other schedulers it can be provided as an
+ * argument to most static operators, or by using the observeOn
+ * or subscribeOn operators.
  */
-of(1, 2, 3).subscribe(observer);
-// of(7, 8, 9, asyncScheduler).subscribe(observer);
-of(4, 5, 6).subscribe(observer);
+// as argument to static operator
 
-/*
- * You can also introduce schedulers by using the
- * observeOn operator. This is equivalent to wrapping
- * next, error, and complete functions in appropriate
- * scheduler.
- *
- * this means everything outsite of the observer/function in the .subscribe()
- * will not be delayed, example the tap operator
- */
-of(7, 8, 9)
-  .pipe(
-    // logging values before scheduler
-    tap((value) => {
-      console.log('observeOn tap: ', value);
-    }),
-    // delay can also be supplied as second argument
-    observeOn(asyncScheduler, 3000)
-    /**
-     * instead of using the delay value,
-     * we should use the delay operator,
-     * because observeOn wraps all notifications in the provider scheduler
-     * meaning error notifications will be delayed.
-     *
-     * delay operator will emit errors immediately
-     */
-  )
-  .subscribe(observer);
+range(1, 5).subscribe(console.log); // synchronous
 
-/*
- * Lastly, you can use schedulers to determine when
- * subscriptions occur by using the subscribeOn
- * operator. This is equivalent to wrapping your entire
- * subscription in a scheduler.
- *
- * it means everything inside of the pipe (this case the tap)
- * and subscribe
+range(1, 5, asapScheduler).subscribe(console.log); // asapScheduler
+
+// of(1,2,3, asapScheduler).subscribe(observer);
+
+// using observeOn
+// of(1,2,3).pipe(
+//   // logging values before scheduler
+//   tap(observer),
+//   observeOn(asapScheduler)
+// ).subscribe(observer);
+
+// using subscribeOn
+// of(1,2,3).pipe(
+//   tap(observer),
+//   subscribeOn(asapScheduler)
+// ).subscribe(observer);
+
+/**
+ * for long running operations it will block the ui until they complete
+ * for this example we can see the browser pauses and then it
+ * finally renders with the final value, this is because the browser is locked
+ * waiting for the Microtask to finish before updating
  */
-of(7, 8, 9)
-  .pipe(
-    tap((value) => {
-      console.log('subscribeOn tap: ', value);
-    }),
-    subscribeOn(asyncScheduler, 6000)
-  )
-  .subscribe(observer);
+
+const counter = document.getElementById('counter');
+const counter2 = document.getElementById('counter2');
+
+range(1, 100000, asapScheduler).subscribe((val) => {
+  counter.innerHTML = val;
+});
+
+// if we remove the asapScheduler we will have the same result
+
+// if we switch to the asyncScheduler
+// we will see the update of the numbers 1by 1
+
+range(1, 1000, asyncScheduler).subscribe((val) => {
+  counter2.innerHTML = val;
+});
