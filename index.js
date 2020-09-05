f1796a516e5a4b743a8423785db8773a64c3cc70
Juan Felipe Teppa <jteppa@rootstack.com>
2020-09-05 17:56:27 -0400

    Use finalize for side effects on completion.

--- index.js
+++ index.js
@@ -1,8 +1,87 @@
-import { of } from 'rxjs';
+// begin lesson code
+import { interval, fromEvent, throwError } from 'rxjs';
+import { take, takeUntil, finalize } from 'rxjs/operators';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of('Hello', 'RxJS').subscribe(console.log);
+// elems
+const counter = document.getElementById('counter');
+
+// streams
+const click$ = fromEvent(document, 'click');
+
+// it completes
+/* const sub = interval(1000)
+  .pipe(take(3))
+  .subscribe({
+    next: (val) => {
+      counter.innerHTML = val;
+    },
+    complete: () => {
+      counter.innerHTML = 'Stopped!';
+    },
+  }); */
+
+// it stops at #2 but it does not complete.
+/* const sub = interval(1000).subscribe({
+  next: (val) => {
+    counter.innerHTML = val;
+  },
+  complete: () => {
+    counter.innerHTML = 'Stopped!';
+  },
+}); */
+
+// calling unsubscribe will not trigger complete callbacks
+/* setTimeout(() => {
+  sub.unsubscribe();
+}, 3000); */
+
+// it will call finalize when unsubscribe.
+// it will also work with take, takeUntil, etc.
+/* const sub = interval(1000)
+  .pipe(
+    finalize(() => {
+      counter.innerHTML = 'Stopped!';
+    })
+  )
+  .subscribe((val) => {
+    counter.innerHTML = val;
+  });
+
+setTimeout(() => {
+  sub.unsubscribe();
+}, 3000); */
+
+/* const sub = interval(1000)
+  .pipe(
+    take(3),
+    finalize(() => {
+      console.log('stopped');
+    })
+  )
+  .subscribe({
+    next: (val) => {
+      counter.innerHTML = val;
+    },
+    complete: () => {
+      console.log('stopped 2');
+    },
+  }); */
+
+const sub = throwError('Error xx')
+  .pipe(
+    take(3),
+    finalize(() => {
+      console.log('stopped');
+    })
+  )
+  .subscribe({
+    next: (val) => {
+      counter.innerHTML = val;
+    },
+    complete: () => {
+      console.log('stopped 2');
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
